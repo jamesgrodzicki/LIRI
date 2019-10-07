0985ef4ba6e16c609b562a187811c40bba4bcbee
James Grodzicki <jamespgrodzicki@gmail.com>
2019-10-06 21:46:28 -0500

    added bandsintown concert search

--- liri.js
+++ liri.js
@@ -16,46 +16,101 @@ inquirer.prompt([
     }
 ]).then(function (ans) {
     if (ans.search === 'concert-this') {
-        // console.log(ans.search);
-        searchTerm();
+        searchConcert();
     } else if (ans.search === 'spotify-this-song') {
         // console.log('spotify-this-song');
-        searchTerm();
+        searchSpot();
     } else if (ans.search === 'movie-this') {
         // console.log(ans.search);
-        searchTerm();
+        searchMovie();
     } else {
         // console.log('do-what-it-says');
         searchTerm();
     }
 });
 
-function searchTerm(){
+function searchConcert() {
     inquirer.prompt([
         {
             type: 'input',
-            name: 'searchSubject',
-            message: 'What woud you like to search for?'
+            name: 'artist',
+            message: 'Enter an artist to look-up an upcoming concert. '
         }
-    ]).then(function(ans){
-        searchSpot(ans.searchSubject);
-        // console.log(result);  
-    })
+    ]).then(function (ans) {
+        const queryURL = 'https://rest.bandsintown.com/artists/' + ans.artist + '/events?app_id=codingbootcamp&date=upcoming';
+        axios({
+            method: 'get',
+            url: queryURL
+        }).then(function (response) {
+            console.log('\nVenue name: ' + response.data[0].venue.name);
+            console.log('--------------------------------------------------------');
+            console.log('Venue Location: ' + response.data[0].venue.city + ', ' + response.data[0].venue.region);
+            console.log('--------------------------------------------------------');
+            console.log('Date: ' + moment(response.data[0].datetime).format('MMM Do YY'));
+
+        }).catch(function (error) {
+            console.log("couldn't find artist");
+        });
+    });
 }
 
-function searchSpot(term){
-    spotify.search({
-        type: 'track',
-        limit: 1,
-        query: term
-    }, function (err, data){
-        if (err) throw err;
-        console.log('\nArtist: ' + data.tracks.items[0].artists[0].name);
-        console.log('--------------------------------------------------------');
-        console.log('Song name: ' + data.tracks.items[0].name);
-        console.log('--------------------------------------------------------');
-        console.log('Link to song: ' + data.tracks.items[0].external_urls.spotify);
-        console.log('--------------------------------------------------------');
-        console.log('Album: ' + data.tracks.items[0].album.name);
+function searchSpot() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'searchSong',
+            message: 'What song would you like to search for? '
+        }
+    ]).then(function (ans) {
+        spotify.search({
+            type: 'track',
+            limit: 1,
+            query: ans.searchSong
+        }).then(function (data) {
+            printSong(data);
+        }).catch(function (err) {
+            spotify.search({
+                type: 'track',
+                limit: 1,
+                query: 'Sign of the Times'
+            }, function (err, data) {
+                if (err) throw err;
+                printSong(data);
+            });
+        });
+    });
+}
+
+function printSong(songData) {
+    console.log('\nArtist: ' + songData.tracks.items[0].artists[0].name);
+    console.log('--------------------------------------------------------');
+    console.log('Song name: ' + songData.tracks.items[0].name);
+    console.log('--------------------------------------------------------');
+    console.log('Link to song: ' + songData.tracks.items[0].external_urls.spotify);
+    console.log('--------------------------------------------------------');
+    console.log('Album: ' + songData.tracks.items[0].album.name);
+}
+
+function searchMovie() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'movie',
+            message: 'Enter a movie. '
+        }
+    ]).then(function(ans){
+        const queryURL = 'http://www.omdbapi.com/?apikey=trilogy&t=' + ans.movie;
+        console.log(queryURL);
+        axios({
+            method: 'get',
+            url: queryURL
+        }).then(function(response){
+            console.log(response.data);
+            console.log('\n```');
+            console.log(response.data.Title);
+            console.log('```');
+        }).catch(function(err){
+            console.log("Couldn't find movie");
+        });
     });
 }
